7bdfa6112ac541af2f050c9aa21d9fbbd9e73574
Alshoo <108189737+Alshoo@users.noreply.github.com>
2025-03-10 17:58:32 +0200

    dash

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/logo-nav.svg" />
+        <link rel="icon" href="./logo-nav.svg" />
       </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
