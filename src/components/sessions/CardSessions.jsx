1cb3555480674018bd6b912c45e6f2957ce6f078
Alshoo <108189737+Alshoo@users.noreply.github.com>
2025-03-21 00:35:20 +0200

    .

--- src/components/sessions/CardSessions.jsx
+++ src/components/sessions/CardSessions.jsx
@@ -39,7 +39,7 @@ function CardSessions({ statusText, buttonText, mode, question_count, created_at
           <li>
             <span className='text-primary font-bold'>Subject </span>: {subject?.name || "Loading..."}
           </li>
-          <li className=' truncate'>
+          <li className=' truncate line-clamp-3'>
             <span className='text-primary font-bold'>Topics </span>: {chapters.length > 0 ? chapters.map((item) => ` ( ${item.name} ) `) : "Loading..."}
           </li>
           <li>
