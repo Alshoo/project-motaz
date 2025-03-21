4c9ad8b945838bfb83cec7a89497a946db6bbffe
Alshoo <108189737+Alshoo@users.noreply.github.com>
2025-03-21 17:34:19 +0200

    store

--- src/components/Store/card-store.jsx
+++ src/components/Store/card-store.jsx
@@ -5,15 +5,17 @@ import Axios from '@/lib/axiosInstance';
 
 function CardStore({ title, pricing_plans, subject_ID, questions_count, chapters, Subject_Image }) {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isCardDisabled, setIsCardDisabled] = useState(false);
   const togglePopup = () => setIsPopupOpen(!isPopupOpen);
-  const [subSubject, setSubSubject] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const subjectRes = await Axios.get("subscriptions");
-        const fetchedSubSubject = subjectRes.data.data.find(sub => sub.subject_id.id === subject_ID)?.subject_id.id;
-        setSubSubject(fetchedSubSubject);
+        const subscription = subjectRes.data.data.find(sub => sub.subject_id.id === subject_ID);
+        if (subscription) {
+          setIsCardDisabled(subscription.pricing_plan_id.free_trial === "1");
+        }
       } catch (err) {
         console.warn(err);
       }
@@ -21,12 +23,11 @@ function CardStore({ title, pricing_plans, subject_ID, questions_count, chapters
     fetchData();
   }, [subject_ID]);
 
-  const isDisabled = subSubject === subject_ID;
-
   return (
-    <div className={`grid grid-cols-1 md:grid-cols-[1fr_2fr_1fr] gap-4 bg-white rounded shadow-lg border border-gray-300 p-4 ${isDisabled ? "opacity-50" : ""}`}>
+    <div className={`grid grid-cols-1 md:grid-cols-[1fr_2fr_1fr] gap-4 bg-white rounded shadow-lg border border-gray-300 p-4 ${isCardDisabled ? "opacity-50" : ""}`}>
       <div className="flex justify-center items-center">
-        <img className="object-cover rounded-lg w-40 md:w-48 h-28 md:h-32"
+        <img
+          className="object-cover rounded-lg w-40 md:w-48 h-28 md:h-32"
           src={Subject_Image ? Subject_Image : "https://storage.googleapis.com/t16t_assets/gyn_prod_logo.jpg"}
           alt={title}
           width={50} height={50} layout="intrinsic" />
@@ -36,13 +37,15 @@ function CardStore({ title, pricing_plans, subject_ID, questions_count, chapters
           <h2 className="text-primary font-bold text-xl md:text-2xl m-0">{title}</h2>
           <h3 className="text-black font-bold text-sm md:text-md m-0">Contains {questions_count} Questions</h3>
           <p className="text-blackOpacity text-sm md:text-base m-0">Choose to buy one of the items and enjoy high-quality content.</p>
-          <button onClick={togglePopup} disabled={isDisabled} className={`mt-4 w-full lg:w-[75%] font-light px-3 py-2 border border-transparent bg-primary text-white rounded-lg shadow-md transition duration-300 ${isDisabled ? "" : "hover:border-primary hover:text-primary hover:bg-transparent"}`}>
-            {isDisabled ? "Subscribed" : "Explore Packages"}
+          <button onClick={togglePopup} disabled={isCardDisabled} className={`mt-4 w-full lg:w-[75%] font-light px-3 py-2 border border-transparent bg-primary text-white rounded-lg shadow-md transition duration-300 ${isCardDisabled ? "" : "hover:border-primary hover:text-primary hover:bg-transparent"}`}>
+            {isCardDisabled ? "Subscribed" : "Explore Packages"}
           </button>
         </div>
       </div>
-      <div className="flex flex-col md:items-end items-center justify-center relative mb-5 md:mb-0 ">
-        <p className="text-black opacity-40 text-sm md:text-base font-semibold absolute top-0">Start at {pricing_plans[0]?.total_price} LYD</p>
+      <div className="flex flex-col md:items-end items-center justify-center relative mb-5 md:mb-0">
+        <p className="text-black opacity-40 text-sm md:text-base font-semibold absolute top-0">
+          Start at {pricing_plans[0]?.total_price} LYD
+        </p>
       </div>
       {isPopupOpen && (
         <div className="fixed inset-0 bg-gray-200 bg-opacity-50 flex justify-center items-center z-50 backdrop-blur-lg">
