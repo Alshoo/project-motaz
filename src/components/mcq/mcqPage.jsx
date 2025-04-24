d75f0c76b7a8f0c58880f8d045ab96ab9929f671
Alshoo <108189737+Alshoo@users.noreply.github.com>
2025-04-24 15:14:58 +0200

    .

--- src/components/mcq/mcqPage.jsx
+++ src/components/mcq/mcqPage.jsx
@@ -358,7 +358,9 @@ function McqPageContent() {
             </div>
 
 
-            <button
+      {
+            resultDetails.total < resultDetails.answer ? (
+              <button
               onClick={
               ()=>{
                 if(questDet.current_page < questDet.total){
@@ -374,13 +376,38 @@ function McqPageContent() {
 
               }
               }
-              disabled={resultDetails.total < resultDetails.answer}
+              disabled
               className="h-8 md:h-[50px] bg-primary text-white px-2 md:px-7 py-0 md:py-4 rounded-full flex justify-center items-center text-xs md:text-base"
             >
               {questDet.current_page >= questDet.total
-                ? "Finish"
+                ?"Finish "
                 :"Next"}
             </button>
+            ) : (
+              <button
+              onClick={
+              ()=>{
+                if(questDet.current_page < questDet.total){
+                  if (mode === "question") {
+                    // handleNextInQuestionMode();
+                    fetchQuest(questDet.current_page + 1);
+                  }else if(mode = "review" ){
+                    handleNextQuestion();
+                  }
+                }else{
+                  handleNextQuestion();
+              }
+
+              }
+              }
+              className="h-8 md:h-[50px] bg-primary text-white px-2 md:px-7 py-0 md:py-4 rounded-full flex justify-center items-center text-xs md:text-base"
+            >
+              {questDet.current_page >= questDet.total
+                ?"Finish"
+                :"Next"}
+            </button>
+            )
+      }
           </div>
         </>
       )}
