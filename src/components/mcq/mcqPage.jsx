0f1e01e8754fca9dc6c5c18718113c7a8dc5dacf
Alshoo <108189737+Alshoo@users.noreply.github.com>
2025-04-24 14:00:38 +0200

    .

--- src/components/mcq/mcqPage.jsx
+++ src/components/mcq/mcqPage.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState, useCallback, Suspense } from "react";
 import Axios from "@/lib/axiosInstance";
-import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import toast from "react-hot-toast";
 import Link from "next/link";
@@ -10,7 +9,6 @@ import "./mcq.css";
 function McqPageContent() {
   const searchParams = useSearchParams();
   const sessionID = searchParams.get("id");
-  const [mode, setMode] = useState("question");
   const [loading, setLoading] = useState(false);
   const [finished, setFinished] = useState(false);
   const [questDet, setQuestDet] = useState({
@@ -24,17 +22,18 @@ function McqPageContent() {
     answers: []
   });
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [showOverallExplanation, setShowOverallExplanation] = useState(false);
   const [detailsVisible, setDetailsVisible] = useState({});
   const [popupImg, setPopupImg] = useState(null);
+
   const constructUrl = useCallback(
     (page = 1) => `exam-Histories/${sessionID}?page=${page}`,
     [sessionID]
   );
+
   const fetchQuest = useCallback(
     async (page = 1) => {
+      setLoading(true);
       try {
-        setLoading(true);
         const res = await Axios.get(constructUrl(page));
         const questions = res.data.data.data;
         if (!questions || questions.length === 0) {
@@ -63,15 +62,8 @@ function McqPageContent() {
           questionSummary: questionData.summary,
           answers: questionData.answers
         });
+        setSelectedAnswer(existingAnswer ? existingAnswer.id : null);
         setDetailsVisible({});
-        setShowOverallExplanation(false);
-        if (existingAnswer) {
-          setSelectedAnswer(existingAnswer.id);
-          setMode("review");
-        } else {
-          setSelectedAnswer(null);
-          setMode("question");
-        }
       } catch (err) {
         console.error(err);
         toast.error("Error fetching question");
@@ -81,12 +73,14 @@ function McqPageContent() {
     },
     [constructUrl]
   );
+
   useEffect(() => {
     if (sessionID) {
       const pageParam = searchParams.get("page") || "1";
       fetchQuest(Number(pageParam));
     }
   }, [sessionID, fetchQuest, searchParams]);
+
   const handleSubmition = async () => {
     try {
       const res = await Axios.put(
@@ -103,28 +97,21 @@ function McqPageContent() {
       toast.error("Error submitting answer");
     }
   };
-  useEffect(() => {
-    if (mode === "question" && selectedAnswer !== null) {
-      (async () => {
-        await handleSubmition();
-        setMode("review");
-      })();
-    }
-  }, [selectedAnswer, mode]);
-  const handleNextInQuestionMode = async () => {
+
+  const handleNext = async () => {
+    if (!selectedAnswer) return;
     await handleSubmition();
-    setMode("review");
-  };
-  const handleNextQuestion = async () => {
     if (questDet.current_page >= questDet.total) {
       setFinished(true);
     } else {
       await fetchQuest(questDet.current_page + 1);
     }
   };
+
   const toggleDetails = (id) => {
     setDetailsVisible((prev) => ({ ...prev, [id]: !prev[id] }));
   };
+
   if (!sessionID) {
     return <div>Error: Session ID not provided.</div>;
   }
@@ -161,203 +148,139 @@ function McqPageContent() {
           <h2 className="mt-2 ml-2 md:ml-4 text-base text-md font-light">
             {questDet.questionText}
           </h2>
-          {mode === "question" && (
-            <div className="mt-5 md:mt-14 space-y-2 md:space-y-4 text-xs sm:text-sm md:text-base shadow-lg rounded-2xl p-2 bg-white overflow-hidden">
-              {questDet.answers.map((ans, i) => {
-                const letter = String.fromCharCode(65 + i);
-                return (
+          <div className="mt-5 md:mt-14 space-y-2 md:space-y-4 text-xs sm:text-sm md:text-base shadow-lg rounded-2xl p-2 bg-white overflow-hidden">
+            {questDet.answers.map((ans, i) => {
+              const letter = String.fromCharCode(65 + i);
+              return (
+                <div
+                  key={i}
+                  onClick={() => setSelectedAnswer(ans.id)}
+                  className={`flex items-center gap-2 py-2 md:py-3 cursor-pointer p-1 md:p-2 rounded-md ${
+                    selectedAnswer === ans.id
+                      ? "bg-blue-100"
+                      : "hover:bg-gray-100"
+                  }`}
+                >
+                  <span className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full font-bold">
+                    {letter}
+                  </span>
+                  <strong className="font-normal text-sm">
+                    {ans.answer_text}
+                  </strong>
+                </div>
+              );
+            })}
+          </div>
+          <div className="mt-5 md:mt-14 text-xs sm:text-sm md:text-base shadow-lg rounded-2xl bg-white overflow-hidden">
+            {questDet.answers.map((ans, i) => {
+              const answerColor = ans.is_correct
+                ? "bg-greenOpacity border-green border-l-4"
+                : "bg-red-100 border-red-600 border-l-4";
+              const answerColorbox = ans.is_correct
+                ? "bg-greenOpacity"
+                : "bg-red-200 ";
+              const answerbgColorbox = ans.is_correct
+                ? "bg-greenWhite"
+                : "bg-red-50 ";
+              const letter = String.fromCharCode(65 + i);
+              return (
+                <div
+                  key={i}
+                  className={`relative ${
+                    selectedAnswer === ans.id ? answerColor : ""
+                  }`}
+                >
                   <div
-                    key={i}
-                    onClick={() => setSelectedAnswer(ans.id)}
-                    className={`flex items-center gap-2 py-2 md:py-3 cursor-pointer p-1 md:p-2 rounded-md ${
+                    className={`flex items-center p-2 md:p-4 ${
                       selectedAnswer === ans.id
-                        ? "bg-blue-100"
-                        : "hover:bg-gray-100"
+                        ? ""
+                        : " border-[#00000000] border-l-4 "
                     }`}
                   >
-                    <span className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full font-bold">
-                      {letter}
-                    </span>
-                    <strong className="font-normal text-sm">
-                      {ans.answer_text}
-                    </strong>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-          {mode === "review" && (
-            <div className="mt-5 md:mt-14 text-xs sm:text-sm md:text-base shadow-lg rounded-2xl bg-white overflow-hidden">
-              {questDet.answers.map((ans, i) => {
-                const answerColor = ans.is_correct
-                  ? "bg-greenOpacity border-green border-l-4"
-                  : "bg-red-100 border-red-600 border-l-4";
-                const answerColorbox = ans.is_correct
-                  ? "bg-greenOpacity"
-                  : "bg-red-200 ";
-                const answerbgColorbox = ans.is_correct
-                  ? "bg-greenWhite"
-                  : "bg-red-50 ";
-                const letter = String.fromCharCode(65 + i);
-                return (
-                  <div
-                    key={i}
-                    className={`relative ${
-                      selectedAnswer === ans.id ? answerColor : ""
-                    }`}
-                  >
-                    <div
-                      className={`flex items-center p-2 md:p-4 ${
+                    <span
+                      className={`w-8 h-8 p-[15px] flex items-center justify-center rounded-full font-bold ${
                         selectedAnswer === ans.id
-                          ? ""
-                          : " border-[#00000000] border-l-4 "
+                          ? ans.is_correct
+                            ? "bg-green text-black"
+                            : "bg-red-200 text-black"
+                          : answerColorbox
                       }`}
                     >
-                      <span
-                        className={`w-8 h-8 p-[15px] flex items-center justify-center rounded-full font-bold ${
-                          selectedAnswer === ans.id
-                            ? ans.is_correct
-                              ? "bg-green text-black"
-                              : "bg-red-200 text-black"
-                            : answerColorbox
+                      {letter}
+                    </span>
+                    <label className="cursor-not-allowed ml-2">
+                      <strong className="font-normal text-sm">
+                        {ans.answer_text}
+                      </strong>
+                    </label>
+                    <button
+                      onClick={() => toggleDetails(ans.id)}
+                      className="ml-auto focus:outline-none"
+                    >
+                      <svg
+                        className={`w-3 h-3 transition-transform opacity-70 ${
+                          detailsVisible[ans.id] ? "rotate-180" : ""
                         }`}
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 10 6"
                       >
-                        {letter}
-                      </span>
-                      <label className="cursor-not-allowed ml-2">
-                        <strong className="font-normal text-sm">
-                          {ans.answer_text}
-                        </strong>
-                      </label>
-                      <button
-                        onClick={() => toggleDetails(ans.id)}
-                        className="ml-auto focus:outline-none"
-                      >
-                        <svg
-                          className={`w-3 h-3 transition-transform opacity-70 ${
-                            detailsVisible[ans.id] ? "rotate-180" : ""
-                          }`}
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 10 6"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="m1 1 4 4 4-4"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                    {detailsVisible[ans.id] && (
-                      <div
-                        className={`mt-2 p-4 shadow-inner text-left text-sm md:text-base prose ${
-                          selectedAnswer === ans.id
-                            ? answerbgColorbox
-                            : "bg-gray-100"
-                        }`}
-                      >
-                        <div
-                          dangerouslySetInnerHTML={{
-                            __html:
-                              ans.description ||
-                              "<ul><li>No details for this answer</li></ul>"
-                          }}
-                          className=""
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="m1 1 4 4 4-4"
                         />
-                        {ans.img && (
-                          <img
-                            src={ans.img}
-                            alt="ERR404"
-                            className="w-[15%] rounded-lg mt-2 cursor-pointer"
-                            onClick={() => setPopupImg(ans.img)}
-                          />
-                        )}
-                      </div>
-                    )}
+                      </svg>
+                    </button>
                   </div>
-                );
-              })}
-            </div>
-          )}
+                  {detailsVisible[ans.id] && (
+                    <div
+                      className={`mt-2 p-4 shadow-inner text-left text-sm md:text-base prose ${
+                        selectedAnswer === ans.id
+                          ? answerbgColorbox
+                          : "bg-gray-100"
+                      }`}
+                    >
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html:
+                            ans.description ||
+                            "<ul><li>No details for this answer</li></ul>"
+                        }}
+                      />
+                      {ans.img && (
+                        <img
+                          src={ans.img}
+                          alt="ERR404"
+                          className="w-[15%] rounded-lg mt-2 cursor-pointer"
+                          onClick={() => setPopupImg(ans.img)}
+                        />
+                      )}
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
           <div className="my-8 flex md:flex-row justify-evenly items-center gap-2 md:gap-4">
             <button
               onClick={() => fetchQuest(questDet.current_page - 1)}
-              className="h-8 md:h-[50px] bg-primary text-white px-2 md:px-7 py-0 md:py-4 rounded-full flex justify-center items-center text-xs md:text-base"
+              disabled={questDet.current_page <= 1}
+              className="h-8 md:h-[50px] bg-primary text-white px-2 md:px-7 py-0 md:py-4 rounded-full flex justify-center items-center disabled:opacity-50 disabled:cursor-not-allowed text-xs md:text-base"
             >
               Back
             </button>
-            <div className="pt-0 pb-0 rounded-full md:text-lg px-2 md:px-10 py-3 text-center relative border shadow-md overflow-hidden">
-              <button
-                onClick={() =>
-                  setShowOverallExplanation((prev) => !prev)
-                }
-                disabled={mode === "question"}
-                className={`flex items-center justify-between gap-2 bg-white p-1 text-gray-900 transition ${
-                  mode === "question"
-                    ? "opacity-50 cursor-not-allowed"
-                    : "cursor-pointer"
-                }`}
-              >
-                <p className="flex items-center text-xs md:text-lg text-stone-500 w-fit py-1 md:py-3 rounded-full">
-                  {showOverallExplanation
-                    ? "Hide Overall Explanation"
-                    : "Show Overall Explanation"}
-                  <svg
-                    className={`w-2.5 h-2.5 ml-1 md:ml-3 transition-transform ${
-                      showOverallExplanation ? "rotate-180" : ""
-                    }`}
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 1 4 4 4-4"
-                    />
-                  </svg>
-                </p>
-              </button>
-            </div>
             <button
-              onClick={
-                mode === "question"
-                  ? handleNextInQuestionMode
-                  : handleNextQuestion
-              }
-              disabled={questDet.current_page >= questDet.total && selectedAnswer === null}
-              className="h-8 md:h-[50px] bg-primary text-white px-2 md:px-7 py-0 md:py-4 rounded-full flex justify-center items-center text-xs md:text-base"
+              onClick={handleNext}
+              disabled={!selectedAnswer}
+              className="h-8 md:h-[50px] bg-primary text-white px-2 md:px-7 py-0 md:py-4 rounded-full flex justify-center items-center disabled:opacity-50 disabled:cursor-not-allowed text-xs md:text-base"
             >
-              {mode === "question"
-                ? "Next"
-                : questDet.current_page >= questDet.total
-                ? "Finish"
-                : "Next"}
+              {questDet.current_page >= questDet.total ? "Finish" : "Next"}
             </button>
           </div>
         </>
-      )}
-      {showOverallExplanation && (
-        <div className="mt-2 border border-gray-200 bg-white p-4 rounded-xl shadow-inner text-sm md:text-base prose">
-          <div
-            dangerouslySetInnerHTML={{
-              __html: questDet.questionDescription
-            }}
-          />
-          {questDet.img && (
-            <img
-              src={questDet.img}
-              alt="ERR404"
-              className="w-[20%] rounded-lg mt-2 cursor-pointer"
-              onClick={() => setPopupImg(questDet.img)}
-            />
-          )}
-        </div>
       )}
       {popupImg && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
