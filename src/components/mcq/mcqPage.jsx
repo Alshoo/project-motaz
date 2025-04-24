d4f92ffc19b17e496672b4535554a36960126d85
Alshoo <108189737+Alshoo@users.noreply.github.com>
2025-04-24 16:01:21 +0200

    .

--- src/components/mcq/mcqPage.jsx
+++ src/components/mcq/mcqPage.jsx
@@ -357,8 +357,7 @@ function McqPageContent() {
             </div>
 
 
-            {
-              resultDetails.total != resultDetails.answer ? (
+            
               <button
                 onClick={() => {
                   if (questDet.current_page < questDet.total) {
@@ -372,30 +371,12 @@ function McqPageContent() {
                   }
                 }}
                 
-                disabled
+                disabled = {resultDetails.total != resultDetails.answer? true : false}
                 className="h-8 md:h-[50px] bg-primary text-white px-2 md:px-7 py-0 md:py-4 rounded-full flex justify-center items-center disabled:opacity-50 text-xs md:text-base"
               >
                 {questDet.current_page >= questDet.total ? "Finish" : "Next"}
               </button>
-            ) : (
-              <button
-                onClick={() => {
-                  if (questDet.current_page < questDet.total) {
-                    if (mode === "question") {
-                      fetchQuest(questDet.current_page + 1);
-                    } else if (mode === "review") {
-                      handleNextQuestion();
-                    }
-                  } else {
-                    handleNextQuestion();
-                  }
-                }}
-                className="h-8 md:h-[50px] bg-primary text-white px-2 md:px-7 py-0 md:py-4 rounded-full flex justify-center items-center text-xs md:text-base"
-              >
-                {questDet.current_page >= questDet.total ? "Finish" : "Next"}
-              </button>
-            )
-          }
+            
 
           </div>
         </>
